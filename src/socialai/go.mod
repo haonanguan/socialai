14a2db00163d9839343cee97fd65f118c7077144
haonanguan <guanhn1214@gmail.com>
2024-08-15 04:18:02 +0000

    changes made

--- src/socialai/go.mod
+++
@@ -1,5 +0,0 @@
-module socialai
-
-go 1.22.6
-
-require github.com/gorilla/mux v1.8.1 // indirect
--- src/socialai/model/model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Post struct {
-	Id      string `json:"id"`
-	User    string `json:"user"`
-	Message string `json:"message"`
-	Url     string `json:"url"`
-	Type    string `json:"type"`
-}
